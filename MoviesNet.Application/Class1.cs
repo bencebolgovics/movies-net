e964bb671f85d9679ea0651f1788d1cbd872d00f
bencebolgovics <bolgovics.bence99@gmail.com>
2025-01-28 20:46:56 +0100

    Create initial solution structure

---
+++ MoviesNet.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MoviesNet.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ MoviesNet.Infrastructure/Data/MoviesNetDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoviesNet.Infrastructure.Data;
+
+public sealed class MoviesNetDbContext(DbContextOptions<MoviesNetDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
